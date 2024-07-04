4888a31a58d1edb344ebd43cf83a9387a7086f72
Damaris Jean Jorge Manzano <damarisjean@Damariss-MacBook-Pro.local>
2024-07-04 11:58:26 +0200

    modal

--- activity.js
+++ activity.js
@@ -1,53 +1,91 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get the modal
-  var modal = document.getElementById("modal");
+  const modal = document.getElementById("modal");
+  const span = document.getElementsByClassName("close")[0];
+  const modalTitle = document.getElementById("modal-title");
+  const modalImage = document.getElementById("modal-image");
+  const modalDescription = document.getElementById("modal-description");
+  const modalBenefits = document.getElementById("modal-benefits");
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  const exerciseInfo = {
+    "Relaxační a zábavné": {
+      title: "Relaxační a zábavné cvičení",
+      image: "images/graphic1.png",
+      description:
+        "Toto cvičení kombinuje relaxační techniky s zábavnými prvky pro uvolnění těla i mysli.",
+      benefits: [
+        "Snižuje stres a úzkost",
+        "Zlepšuje kvalitu spánku",
+        "Zvyšuje celkovou pohodu",
+      ],
+    },
+    "Hooping a zábavné": {
+      title: "Hooping a zábavné cvičení",
+      image: "images/graphic2.png",
+      description:
+        "Hooping je skvělý způsob, jak spojit kardio trénink s zábavou a koordinací.",
+      benefits: [
+        "Posiluje core a zlepšuje rovnováhu",
+        "Spaluje kalorie a tvaruje postavu",
+        "Rozvíjí koordinaci a rytmus",
+      ],
+    },
+    "Streaching a zábavné": {
+      title: "Stretching a zábavné cvičení",
+      image: "images/graphic3.png",
+      description:
+        "Kombinace stretchingu a zábavných prvků pro zlepšení flexibility a nálady.",
+      benefits: [
+        "Zvyšuje flexibilitu a rozsah pohybu",
+        "Snižuje riziko zranění",
+        "Zlepšuje držení těla",
+      ],
+    },
+    "Skákání a zábavné": {
+      title: "Skákání a zábavné cvičení",
+      image: "images/graphic4.png",
+      description:
+        "Energické cvičení kombinující skákání s různými zábavnými aktivitami pro intenzivní kardio trénink.",
+      benefits: [
+        "Zlepšuje kardiovaskulární zdraví",
+        "Posiluje dolní končetiny a core",
+        "Zvyšuje energii a vitalitu",
+      ],
+    },
+  };
 
-  // Get the modal info div
-  var modalInfo = document.getElementById("modal-info");
-
-  // Function to open the modal with the relevant information
   function openModal(info) {
-    modalInfo.innerHTML = info;
+    modalTitle.textContent = info.title;
+    modalImage.src = info.image;
+    modalImage.alt = info.title;
+    modalDescription.textContent = info.description;
+    modalBenefits.innerHTML = info.benefits
+      .map((benefit) => `<li>${benefit}</li>`)
+      .join("");
     modal.style.display = "block";
   }
 
-  // Add click events for each graphic
-  var graphics = document.querySelectorAll(".box-click");
-  console.log("Graphics found:", graphics);
-
-  if (graphics.length === 0) {
-    console.error("No graphic elements found!");
-  }
+  const graphics = document.querySelectorAll(".box-click");
 
   graphics.forEach(function (graphic) {
     graphic.addEventListener("click", function () {
-      var info = "";
+      let exerciseType = "";
       if (graphic.querySelector(".graphic1")) {
-        info =
-          "<h3>Relaxační a zábavné</h3><p>Information about Relaxační a zábavné.</p>";
+        exerciseType = "Relaxační a zábavné";
       } else if (graphic.querySelector(".graphic2")) {
-        info =
-          "<h3>Hooping a zábavné</h3><p>Information about Hooping a zábavné.</p>";
+        exerciseType = "Hooping a zábavné";
       } else if (graphic.querySelector(".graphic3")) {
-        info =
-          "<h3>Streaching a zábavné</h3><p>Information about Streaching a zábavné.</p>";
+        exerciseType = "Streaching a zábavné";
       } else if (graphic.querySelector(".graphic4")) {
-        info =
-          "<h3>Skákání a zábavné</h3><p>Information about Skákání a zábavné.</p>";
+        exerciseType = "Skákání a zábavné";
       }
-      openModal(info);
+      openModal(exerciseInfo[exerciseType]);
     });
   });
 
-  // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
     modal.style.display = "none";
   };
 
-  // When the user clicks anywhere outside of the modal, close it
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
